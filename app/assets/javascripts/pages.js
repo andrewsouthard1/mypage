723ee44a459000f511cf0646a9d951fae56e290b
Andrew <andrewsouthard111@gmail.com>
2017-05-31 13:23:52 -0400

    First commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+  showBio();
+}
+
+function jsTest() {
+  document.getElementById('js-test').style.color ='red';
+}
+
+function showBio() {
+  document.getElementById('projects').style.display ='none';
+  document.getElementById('blog').style.display ='none';
+  document.getElementById('contact').style.display ='none';
+  document.getElementById('bio').style.display = '';
+  $("iframe").each(function() { 
+        var src= $(this).attr('src');
+        $(this).attr('src',src);  
+  });
+}
+
+function showProjects() {
+  document.getElementById('projects').style.display = '';
+  document.getElementById('blog').style.display ='none';
+  document.getElementById('contact').style.display ='none';
+  document.getElementById('bio').style.display ='none';
+}
+
+function showBlog() {
+  document.getElementById('projects').style.display ='none';
+  document.getElementById('blog').style.display = '';
+  document.getElementById('contact').style.display ='none';
+  document.getElementById('bio').style.display ='none';
+  $("iframe").each(function() { 
+        var src= $(this).attr('src');
+        $(this).attr('src',src);  
+  });
+}
+
+function showContact() {
+  document.getElementById('projects').style.display ='none';
+  document.getElementById('blog').style.display ='none';
+  document.getElementById('contact').style.display = '';
+  document.getElementById('bio').style.display ='none';
+  $("iframe").each(function() { 
+        var src= $(this).attr('src');
+        $(this).attr('src',src);  
+  });
+}
