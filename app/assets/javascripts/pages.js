c2d449877159cb707694f4f4da60f5c206cdad74
Andrew <andrewsouthard111@gmail.com>
2017-05-31 21:00:31 -0400

    Changed project descriptions

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,16 +1,13 @@
-window.onload = function() {
-  showBio();
-}
 
 function jsTest() {
   document.getElementById('js-test').style.color ='red';
 }
 
 function showBio() {
-  document.getElementById('projects').style.display ='none';
+  document.getElementById('bio').style.display = '';
+    document.getElementById('projects').style.display ='none';
   document.getElementById('blog').style.display ='none';
   document.getElementById('contact').style.display ='none';
-  document.getElementById('bio').style.display = '';
   $("iframe").each(function() { 
         var src= $(this).attr('src');
         $(this).attr('src',src);  
@@ -19,6 +16,7 @@ function showBio() {
 
 function showProjects() {
   document.getElementById('projects').style.display = '';
+  document.getElementById('projects').style.opacity = 1;
   document.getElementById('blog').style.display ='none';
   document.getElementById('contact').style.display ='none';
   document.getElementById('bio').style.display ='none';
